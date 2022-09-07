0c205772df7c20549d06b646e7bec8676d2a69a8
TheTykon <ragetykon@gmail.com>
2022-09-08 00:09:09 +0200

    fix some shit

--- src/js/game.js
+++ src/js/game.js
@@ -1,35 +1,29 @@
-if($(".play")){
+let player = '.player';
+let obstacle = '.obst';
+let score = 0;
 
-    let player = '.player';
-    let obstacle = '.obst';
-    let score = 0;
-
-    function yeet(){
-        if ($(player).hasClass("jumping")){
-            return;
-        }
-        $(player).addClass("jumping");
-        setTimeout(function () {
-            $(player).removeClass("jumping");
-        }, 300);
+function yeet(){
+    if ($(player).hasClass("jumping")){
+        return;
     }
-
-    let DOA = setInterval(function () {
-        let plVal = parseInt($(player).css("top"));
-        let obstPos = parseInt($(obstacle).css("left"));
-        //console.log(parseInt($(player).css("top")));
-        if (obstPos<85 && obstPos>-85 && plVal>=130){
-            $(obstacle).css("animation", "none");
-            $(obstacle).css("display", "none");
-            alert("u ded boi - Score: " + Math.floor(score/100));
-            score = 0;
-            $(obstacle).css("animation", "evil_teko 1.5s infinite linear");
-            $(obstacle).css("display", "block");
-
-        } else{
-            score++;
-            $(".score span").html(Math.floor(score/100));
-        }
-    }, 15);
-
+    $(player).addClass("jumping");
+    setTimeout(function () {
+        $(player).removeClass("jumping");
+    }, 500);
 }
+
+let DOA = setInterval(function () {
+    let plVal = parseInt($('.player').css("top"));
+    let obstPos = $('.obst').position().left;
+    //console.log($('.player').position().top);
+    if (obstPos<60 && obstPos>-40 && plVal>=220){
+        $(obstacle).css("animation", "none");
+        alert("u ded boi - Score: " + Math.floor(score/100));
+        score = 0;
+        $(obstacle).css("animation", "evil_teko 1s infinite linear");
+
+    } else{
+        score++;
+        $(".score span").html(Math.floor(score/100));
+    }
+}, 10);
